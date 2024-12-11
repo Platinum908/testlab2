c6c4c4167635f35a160f5131d8ac1b3bf4ac5a3a
Platinum908 <estnuntius@gmail.com>
2024-12-11 20:21:20 +0200

    Добавьте файлы проекта.

---
+++ testlab2/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testlab2
+{
+    public class Class1
+    {
+        public class SphereCalculations
+        {
+            const double Strength = 3e6; // Напряженость поля
+            const double Epsilon = 8.85e-12; // Электрическая постоянная
+
+            public double CalculateMaxCharge(double radius)
+            {
+                return Strength * radius * Epsilon;
+            }
+
+            public double CalculatePotential(double charge, double radius)
+            {
+                return charge / (4 * Math.PI * Epsilon * radius);
+            }
+        }
+    }
+}
---
+++ testlab2/Program.cs
@@ -0,0 +1,22 @@
+﻿using static testlab2.Class1;
+
+Console.WriteLine("Введите радиус 1 шара:");
+double r1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите радиус 2 шара:");
+double r2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите радиус 3 шара:");
+double r3 = Convert.ToDouble(Console.ReadLine());
+
+SphereCalculations calc = new SphereCalculations();
+
+double[] radius = { r1, r2, r3 };
+
+for (int i = 0; i < radius.Length; i++)
+{
+    double charge = calc.CalculateMaxCharge(radius[i]);
+    double potential = calc.CalculatePotential(charge, radius[i]);
+
+    Console.WriteLine($"Шар {i + 1}: Максимальный заряд = {charge:E2} Кл, Потенциал = {potential:E2} В");
+}
